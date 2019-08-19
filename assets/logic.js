8724181e1b24b14e03c578fc1123090b8753a360
Zion Brown <zabs95@gmail.com>
2019-08-19 12:07:52 -0400

    Can add new buttons, however the ajax response is returning an error

---
+++ assets/logic.js
@@ -0,0 +1,62 @@
+
+var topics = ["cats", "dogs", "fish", "snail"];
+
+var addGif = function() {
+
+    var searchTerm = $(this).attr("data-name");
+
+    var key = "pZgX54Wez0czcsiYa1Nuj0pr6CaMSzwl"
+
+    var gifNumber = 3
+
+    var queryURL = "api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + key + "&limit=" + gifNumber
+
+    console.log(queryURL)
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+
+        console.log(response)
+        
+
+      });
+}
+
+var addButtons = function() {
+
+    $("#buttonBox").html("")
+
+    for (var i = 0; i < topics.length; i++) { //loops through all topics
+
+        var button = $("<button>");
+
+        button.addClass("button");
+
+        button.attr("data-name", topics[i]);
+
+        button.text(topics[i]);
+
+        $("#buttonBox").append(button);
+
+    }
+
+}
+
+$("#add-topic").on("click", function(event) {
+
+    event.preventDefault();
+
+    var newTopic = $("#input").val().trim(); //gets the string entered into the textbox and removes whitespace
+
+    topics.push(newTopic);
+
+    $("#input").val("")
+
+    addButtons();
+
+})
+    
+$(document).on("click", ".button", addGif);
+
+addButtons(); //inital run to add the starting buttons
